1bd41695f612cb8922a03be3c5e54889a178efb0
ksx <kd.stajkowski@gmail.com>
2022-07-04 13:44:32 +0200

    basic controller

---
+++ src/main/java/pl/coderslab/controller/DonationController.java
@@ -0,0 +1,15 @@
+package pl.coderslab.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**Spring 4.0 introduced the @RestController annotation in order to simplify the creation of RESTful web services.
+ * combines @Controller and @ResponseBody, which eliminates the need to annotate every request handling method of the controller class
+ * with the @ResponseBody annotation.
+ */
+
+@RestController
+public class DonationController {
+
+
+
+}
